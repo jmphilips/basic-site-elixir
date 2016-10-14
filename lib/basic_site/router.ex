959bc6940c46b5b1f185068267b123d3b49e9fab
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-13 20:42:40 -0500

    Added a wildcard route that takes an argument from the command line and returns the argument

--- lib/basic_site/router.ex
+++ lib/basic_site/router.ex
@@ -1,10 +1,21 @@
 defmodule BasicSite.Router do
     use Plug.Router
-
+    use Plug.Debugger
+    
+    plug Plug.Logger
     plug :match
     plug :dispatch
 
     get "/" do
         send_resp(conn, 200, "Hi from plug")
     end
+
+    get "/bug" do
+        raise "WAT"
+    end
+
+     get "/:name" do
+        send_resp(conn, 200, EEx.eval_file())
+    end
+
 end
